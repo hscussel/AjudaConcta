f5691c087d19b3ac255818a19e7a1317069dd4bf
hscussel <hscussel@hotmail.com>
2024-10-21 15:02:57 -0300

    new commit

---
+++ js/admin-nav.js
@@ -0,0 +1,8 @@
+document.getElementById('dashboardNav').addEventListener('click', function() {
+    window.location.href = '../html/dashboards.html';
+});
+
+document.getElementById('userNav').addEventListener('click', function() {
+    window.location.href = '../html/user.html';
+});
+
