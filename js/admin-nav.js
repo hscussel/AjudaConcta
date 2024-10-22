0064bfbe6f5c3c508fe3e8958fb46f10f67437fd
hscussel <hscussel@hotmail.com>
2024-10-22 15:41:44 -0300

    new commit

--- js/admin-nav.js
+++ js/admin-nav.js
@@ -6,3 +6,14 @@ document.getElementById('userNav').addEventListener('click', function() {
     window.location.href = '../html/user.html';
 });
 
+document.getElementById('catastrofesNav').addEventListener('click', function() {
+    window.location.href = '../html/catastrofes.html';
+});
+
+document.getElementById('donationNav').addEventListener('click', function() {
+    window.location.href = '../html/doacoes.html';
+});
+
+document.getElementById('deliveryNav').addEventListener('click', function() {
+    window.location.href = '../html/entregas.html';
+});
