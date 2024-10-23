ded9b953cd353a320d2d923d7e2278b9af8a088f
hscussel <hscussel@hotmail.com>
2024-10-23 13:29:22 -0300

    new commit

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
 document.getElementById('loginButton').addEventListener('click', function() {
     window.location.href = '../html/login.html';
 });
+
---
+++ js/loginNav.js
@@ -0,0 +1,6 @@
+document.getElementById('singUp').addEventListener('click', function() {
+    window.location.href = '../html/singup.html';
+});
+document.getElementById('logIn').addEventListener('click', function() {
+    window.location.href = '../html/login.html';
+});
