b40fd3a18b741bec444726f4184c8dadee3e08ed
Henrique Scussel <hscussel@hotmail.com>
2024-10-28 21:42:48 -0300

    new commit

---
+++ js/usuariosNav.js
@@ -0,0 +1,9 @@
+document.getElementById('acompanharNav').addEventListener('click', function() {
+    window.location.href = '../html/acompanhar-user.html';
+});
+document.getElementById('doarNav').addEventListener('click', function() {
+    window.location.href = '../html/doar-user.html';
+});
+document.getElementById('perfilNav').addEventListener('click', function() {
+    window.location.href = '../html/perfil-user.html';
+});
